b9f02df01f8054034a2bb01867c2b22d6ca6b2ce
WaltMorrow <wmorrow283@noscihigh.org>
2021-03-29 04:23:04 +0000

    Answers

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ Declare a variable named myArray and assign it to the value of an array literal
 Using a for loop, and the push method, push into the array the numbers 1 thru 20;
 For loops (cont...)
 */
+var myArray = [];
 
-
-
+for (let i=0; i<=20; i++){
+  console.log(print(i))
+}
 
 
 
@@ -22,7 +24,12 @@ For loops (cont...)
 Inside the function printer, create a for loop that prints all of the values in 
 myArray in reverse to the console
 */
+for (let i = 0; i <= 0; i++){
 
+  if(i%1 !==0){
+    console.log(myArray)
+  }
+}
 
 
 
@@ -41,7 +48,12 @@ Using dot notation, add three keys, first, second, and third and assign to each 
 the value of a 10, 20, and 30
 */
 
+var myObject = {
 
+  first.key. = 10
+  second.key. = 20
+  third.key. = 30
+}
 
 
 
@@ -53,3 +65,9 @@ the value of a 10, 20, and 30
 /*
 Using a for in loop, and the addition assignment(+=) operator, add five to each value in myObject
 */
+
+for(var i = myObject [70]; <= number; i++){
+  if(i+=5!==0){
+    console.log(myObject(i))
+  }
+}
